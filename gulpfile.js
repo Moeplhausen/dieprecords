651aca4019adb3486c02eccb9595d77145132943
Moepl <github@mcom.moepl.eu>
2016-10-22 18:40:19 +0200

    Improved look of the navigation

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,5 @@
 const elixir = require('laravel-elixir');
 
-/*
-elixir(function(mix) {
-    mix.scriptsIn('resources/assets/js');
-    mix.styles([
-        'app.css',
-        'bootstrap.min.css',
-        'ekko-lightbox.min.css',
-        'jquery.dynatable.css',
-        'tanks.css',
-        'ubuntufont.css',
-    ]);
-    mix.copy('resources/assets/images', 'public/images');
-    mix.version(['css/all.css', 'js/all.js','public/images']);
-});
-*/
-
-// gulpfile.js
 
 elixir(function(mix) {
     mix.sass('app.scss')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,38 +13,40 @@
     <link rel="stylesheet" href="{{  elixir('css/app.css')}}">
 
 
-
     <style>
         @yield('customstyle')
     </style>
 </head>
 <body>
 <div class="container">
-
-    <nav class="navbar topmenu record-submit">
-        <div class="col-sm-6">
-            @yield('leftnavitem')
+    <nav class="navbar navbar-light">
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbar-header"
+                aria-controls="navbar-header" aria-expanded="false" aria-label="Toggle navigation"></button>
+        <div class="collapse navbar-toggleable-xs" id="navbar-header">
+            <span class="nav navbar-nav">
+                @yield('leftnavitem')
+                </span>
+            <span class="float-xs-right">
+                @if(Auth::guest())
+                    <button type="button" class="btn btn-primary btn-lg btn-diep diep-gradient-blue" data-toggle="modal"
+                            data-target="#managerlogin">Manager login
+                    </button>
+                @else
+                    <button type="button" class="btn btn-primary btn-lg btn-diep diep-gradient-blue"
+                            onclick=window.location.href="/submissions">
+                        Submitted Records
+                    </button>
+                    <button type="button" class="btn btn-primary btn-lg btn-diep diep-gradient-red"
+                            onclick="$('#logout-form').submit()">
+                        Logout
+                    </button>
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                @endif
+            </span>
         </div>
-        <div class="col-sm-6">
-            @if(Auth::guest())
-                <button type="button" class="btn btn-primary btn-lg btn-diep diep-gradient-blue" data-toggle="modal"
-                        data-target="#managerlogin">Manager login
-                </button>
-            @else
-                <button type="button" class="btn btn-primary btn-lg btn-diep diep-gradient-blue" onclick=window.location.href="/submissions">Submitted Records</button>
-                <button type="button" class="btn btn-primary btn-lg btn-diep diep-gradient-red" onclick="$('#logout-form').submit()">
-                    Logout
-                </button>
-                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-
-            @endif
-        </div>
-
-    </nav>
-
-
+    </nav> <!-- /navbar -->
     @yield('content')
 </div>
 
@@ -53,7 +55,7 @@
 
 <script>
 
-    $(function() {
+    $(function () {
         $.ajaxSetup({
             headers: {
                 'X-CSRF-Token': $('meta[name="_token"]').attr('content')
@@ -68,8 +70,7 @@
     });
 
 
-
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
         event.preventDefault();
         $(this).ekkoLightbox({
             alwaysShowClose: true,
