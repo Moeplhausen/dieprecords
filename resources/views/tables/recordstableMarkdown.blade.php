950f92c2fc8695c40dddef2178a0dce36ea81d1b
Moepl <github@mcom.moepl.eu>
2016-11-07 18:55:26 +0100

    little markdown fix

--- resources/views/tables/recordstableMarkdown.blade.php
+++ resources/views/tables/recordstableMarkdown.blade.php
@@ -33,9 +33,9 @@ Class|@foreach ($gamemodes as $gamemode)@if($loop->last){{$gamemode->name}}@else
 @endforeach
     **Dead Classes** @foreach ($gamemodes as $gamemode)| @endforeach
 
-~~Mega Smasher~~ | [☢ 1.11M](http://imgur.com/gallery/lbyOh) | [uvwxyz 368k](http://imgur.com/AsSX0Nm) | X
-~~Predator~~ | [YOBA 283k](https://cdn.discordapp.com/attachments/222132681394225155/226264896185040896/YOBA283137.png) | [✘ 330k](http://m.imgur.com/a/ViHyS) | X
-~~X Hunter~~ | [/u/JezzPanda 548k](http://imgur.com/a/S36ZL) | [YOBA 417k](https://cdn.discordapp.com/attachments/214953543558234115/220044236479791105/YOBA417176.png) | [{❂}LinkNPkmn 206k](https://cdn.discordapp.com/attachments/220900206256848897/220965503139577856/Screenshot_20160815-154316.png)
+~~Mega Smasher~~ | [☢ 1.11M](http://imgur.com/gallery/lbyOh) | [uvwxyz 368k](http://imgur.com/AsSX0Nm) | X| X
+~~Predator~~ | [YOBA 283k](https://cdn.discordapp.com/attachments/222132681394225155/226264896185040896/YOBA283137.png) | [✘ 330k](http://m.imgur.com/a/ViHyS) | X| X
+~~X Hunter~~ | [/u/JezzPanda 548k](http://imgur.com/a/S36ZL) | [YOBA 417k](https://cdn.discordapp.com/attachments/214953543558234115/220044236479791105/YOBA417176.png) | [{❂}LinkNPkmn 206k](https://cdn.discordapp.com/attachments/220900206256848897/220965503139577856/Screenshot_20160815-154316.png)| X
 &nbsp;
 
 MOBILE
