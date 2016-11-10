484d1effad7d00c73aefa6ad6d2355466ee9b039
Moepl <github@mcom.moepl.eu>
2016-11-10 14:08:56 +0100

    Added meta description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="_token" content="{{ csrf_token() }}">
+    <meta name="description" content="Unofficial World Records site for the game diep.io Submit your records here!">
 
     <title>@yield('title')</title>
 
