d50e62a4070747e1d9aa71611563c306edeb4cef
Moepl <github@mcom.moepl.eu>
2017-12-30 07:45:17 +0100

    added skimmer v1 to dead tanks

--- resources/views/markdown/recordstableMarkdown.blade.php
+++ resources/views/markdown/recordstableMarkdown.blade.php
@@ -2,6 +2,7 @@
     @include('tables.markdowntablehelper',['gamemodes'=>$gamemodesDesktop,'allrecords'=>$allrecordsDesktop])
     **Dead Classes** @foreach ($gamemodesDesktop as $gamemode)| @endforeach
 
+~~Skimmer v1~~ | [An unnamed tank 1.35M](https://imgur.com/a/fasM0) | [UVEK 1.89M](https://www.youtube.com/watch?v=_L2qfEZB3gU) | [MOUTARDE 1.57M](https://imgur.com/a/e95Kv)| [Adasba 992K](https://www.youtube.com/watch?v=pROyHBsab3k)
 ~~Mega Smasher~~ | [☢ 1.11M](http://imgur.com/gallery/lbyOh) | [uvwxyz 368k](http://imgur.com/AsSX0Nm) | X| X
 ~~Predator~~ | [YOBA 283k](https://cdn.discordapp.com/attachments/222132681394225155/226264896185040896/YOBA283137.png) | [✘ 330k](http://m.imgur.com/a/ViHyS) | X| X
 ~~X Hunter~~ | [/u/JezzPanda 548k](http://imgur.com/a/S36ZL) | [YOBA 417k](https://cdn.discordapp.com/attachments/214953543558234115/220044236479791105/YOBA417176.png) | [{❂}LinkNPkmn 206k](https://cdn.discordapp.com/attachments/220900206256848897/220965503139577856/Screenshot_20160815-154316.png)| X
