19513a7a4bcd2ed0909feceac66258a2e0f28d4c
Moepl <github@mcom.moepl.eu>
2018-04-28 03:31:17 +0200

    sql fix

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -213,7 +213,7 @@ ORDER  BY score ASC
         /*
 * Get Best Tanks when you sum the gamemodes up
 */
-        $topRecords = DB::select("SELECT approvedrecords.id, approvedrecords.name,users.name as approvername,approvedrecords.score,gamemodes.name as gamemode,tanks.tankname as tank,proofs.submittedlink,proofs.created_at FROM approvedrecords inner join proofs on approvedrecords.id=proofs.id INNER JOIN tanks on approvedrecords.tank_id=tanks.id INNER JOIN gamemodes ON approvedrecords.gamemode_id=gamemodes.id INNER JOIN users ON proofs.approver_id=users.id WHERE gamemodes.mobile=0 ORDER BY `approvedrecords`.`id`  DESC LIMIT 100");
+        $topRecords = DB::select("SELECT approvedrecords.id, approvedrecords.name,users.name as approvername,approvedrecords.score,gamemodes.name as gamemode,tanks.tankname as tank,proofs.submittedlink,proofs.created_at FROM approvedrecords inner join proofs on approvedrecords.id=proofs.id INNER JOIN tanks on approvedrecords.tank_id=tanks.id INNER JOIN gamemodes ON approvedrecords.gamemode_id=gamemodes.id INNER JOIN users ON proofs.approver_id=users.id WHERE gamemodes.mobile=0 ORDER BY `approvedrecords`.`score`  DESC LIMIT 100");
         for ($i = 0; $i < count($topRecords); $i++) {
             $record = $topRecords[$i];
             $record->row = $i + 1;
@@ -451,7 +451,7 @@ Be aware that for a records with multiple proof-links we get a result each
         if ($worldRecordSubmission && $currentbestone && $currentbestone->score >= $request->score) {
             $worldRecordSubmission = false;
 
-            $top100Record = DB::select("SELECT approvedrecords.id, approvedrecords.name,users.name as approvername,approvedrecords.score,gamemodes.name as gamemode,tanks.tankname as tank,proofs.submittedlink,proofs.created_at FROM approvedrecords inner join proofs on approvedrecords.id=proofs.id INNER JOIN tanks on approvedrecords.tank_id=tanks.id INNER JOIN gamemodes ON approvedrecords.gamemode_id=gamemodes.id INNER JOIN users ON proofs.approver_id=users.id WHERE gamemodes.mobile=0 ORDER BY `approvedrecords`.`id`  DESC LIMIT 99,1");
+            $top100Record = DB::select("SELECT approvedrecords.id, approvedrecords.name,users.name as approvername,approvedrecords.score,gamemodes.name as gamemode,tanks.tankname as tank,proofs.submittedlink,proofs.created_at FROM approvedrecords inner join proofs on approvedrecords.id=proofs.id INNER JOIN tanks on approvedrecords.tank_id=tanks.id INNER JOIN gamemodes ON approvedrecords.gamemode_id=gamemodes.id INNER JOIN users ON proofs.approver_id=users.id WHERE gamemodes.mobile=0 ORDER BY `approvedrecords`.`score`  DESC LIMIT 99,1");
 
             if ($gamemodeinfo->mobile == true or ($top100Record && $request->score < $top100Record[0]->score))
                 if ($apiRequest)
